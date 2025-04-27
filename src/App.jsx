d735df7feed92b098889ae1901b4a2050facc1ce
Muhammad Sayyid Rafee Djamil <msayyid.rafeed@gmail.com>
2025-04-27 17:56:46 +0700

    Siap pakai

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">Aku jelajah budaya</h1>
+    </div>
   )
 }
 
