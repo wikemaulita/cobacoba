9c2d74970e77392c196e809cf31a78ffdad684bf
Muhammad Sayyid Rafee Djamil <msayyid.rafeed@gmail.com>
2025-05-05 18:57:52 +0700

    Navbar, Home, Login, Register

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+      <div className="p-8 text-center">
+        <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
+        <p className="mt-4 text-gray-600">Halaman yang kamu cari tidak ditemukan.</p>
+      </div>
+    );
+  };
+  
+  export default NotFound;
+  
