0fc6968459f251181552de9939fa2ee7fd2affe7
Wike Maulita <wika.maulita@example.com>
2025-05-05 23:20:59 +0700

    membuat eventbudaya

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
+import Home from './pages/home'
+import EventBudaya from './pages/eventbudaya'
+import DetailEvent from './pages/Detailevent'
+import Pulau from './pages/pulau'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -14,8 +18,12 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/eventbudaya" element={<EventBudaya/>} />
+            <Route path="/Detailevent" element={<DetailEvent />} />
+            <Route path="/pulau" element={<Pulau />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} /> 
           </Routes>
         </main>
         <Footer />
--- src/pages/eventbudaya.jsx
+++ src/pages/eventbudaya.jsx
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
+import eventImage from "../assets/images/eventImage.webp";
+
 const EventBudaya = () => {
-    return (
-        <div className="p-8">
-        <h1 className="text-4xl font-bold">Event Budaya</h1>
-      </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen pt-24">
+      {/* Hero Section */}
+      <section
+        className="relative flex items-center justify-center py-32 bg-cover bg-center"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${eventImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          minHeight: "85vh",
+        }}
+      >
+      <div className="container mx-auto px-4 text-left text-white">
+      <h1 className="text-5xl font-bold mb-4">Lebaran Betawi 2025</h1>
+        <p className="text-xl mb-8">
+           Ikuti kemeriahan Lebaran Betawi 2025 dengan berbagai acara menarik, mulai dari bazaar kuliner hingga pertunjukan seni tradisional. Bergabunglah bersama kami untuk merayakan budaya Betawi yang kaya dan beragam.
+         </p>
+      <Link to="/Detailevent" className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full transition duration-300">
+         Lihat Event
+       </Link>
+     </div>
+      </section>
+    </div>
+  );
 };
 
 export default EventBudaya;
