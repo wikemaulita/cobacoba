253f4e43d3e8abf271a6e8a1ec425825d247adac
Wike Maulita <wika.maulita@example.com>
2025-06-06 15:17:56 +0700

    favicon

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,15 +1,20 @@
-// src/layouts/UserLayout.jsx
+import { Helmet } from "react-helmet";
 import UserSidebar from "@/components/user/sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function UserLayout() {
   return (
-    // PERUBAHAN: Latar belakang utama menjadi abu-abu muda
-    <div className="flex h-screen bg-slate-100 dark:bg-slate-900">
-      <UserSidebar />
-      <div className="flex-1 overflow-auto p-4 sm:p-6 md:p-8">
-        <Outlet /> {/* Child routes will render here */}
+    <>
+      <Helmet>
+        <link rel="icon" href="/JB.svg" type="image/svg+xml" />
+      </Helmet>
+
+      <div className="flex h-screen bg-slate-100 dark:bg-slate-900">
+        <UserSidebar />
+        <div className="flex-1 overflow-auto p-4 sm:p-6 md:p-8">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
