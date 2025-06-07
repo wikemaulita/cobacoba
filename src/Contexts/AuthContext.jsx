9d6806c624184ce55fd80cb3f1234d3166e668ca
Muhammad Sayyid Rafee Djamil <msayyid.rafeed@gmail.com>
2025-06-07 08:11:57 +0700

    Miss 1 authcontext

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -2,7 +2,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '@/hooks/use-toast';
-// Import API functions
 import { loginUser, registerRegularUser } from '@/lib/api'; 
 
 const AuthContext = createContext(null);
